4e3046bee1f2de0aac3e195042fbcc5520c33bdc
vtyagi490 <55434903+vtyagi490@users.noreply.github.com>
2024-01-17 04:10:15 +0000

    new change

--- streamlit_agent/chat_pandas_df.py
+++ streamlit_agent/chat_pandas_df.py
@@ -2,8 +2,8 @@ from langchain.agents import AgentType
 from langchain.agents import create_pandas_dataframe_agent
 # from langchain_experimental.agents.agent_toolkits import create_pandas_dataframe_agent
 from langchain.callbacks import StreamlitCallbackHandler
-#from langchain.chat_models import ChatOpenAI
-from langchain_community.chat_models import ChatOpenAI
+from langchain.chat_models import ChatOpenAI
+# from langchain_community.chat_models import ChatOpenAI
 import streamlit as st
 import pandas as pd
 import os
